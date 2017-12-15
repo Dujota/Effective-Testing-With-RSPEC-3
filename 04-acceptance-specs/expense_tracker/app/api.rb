c82b4f131215cfed4cec827e4d8a849116f0dc40
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-12-15 15:35:45 -0500

    added a mock instance doubl e of the ledger and created a record result struct class to sumulate what the response at the post expense would be

--- 04-acceptance-specs/expense_tracker/app/api.rb
+++ 04-acceptance-specs/expense_tracker/app/api.rb
@@ -4,13 +4,13 @@ require "json"
 module ExpenseTracker
   class API < Sinatra::Base
 
-    def initializer(ledger: Ledger.new) # default value so callers can just say API.new and get the default
+    def initialize(ledger: Ledger.new) # default value so callers can just say API.new and get the default
       @ledger = ledger
       super() # rest of initialization from Sinatra
     end
 
-    # Later, callers do this
-    app = API.new(ledger: Ledger.new)
+    # Later, callers do this ledger: Ledger.new
+    app = API.new()
 
     post '/expenses' do
       JSON.generate('expense_id' => 42)
