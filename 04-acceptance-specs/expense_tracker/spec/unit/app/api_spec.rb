5744f210f7f9759a40f857b7c28e67fb96d769b3
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-12-18 14:09:44 -0500

    compelted unit specs for validation and not validated responses, integrated the fail and pass response on the api.rb file for post expenses

--- 04-acceptance-specs/expense_tracker/spec/unit/app/api_spec.rb
+++ 04-acceptance-specs/expense_tracker/spec/unit/app/api_spec.rb
@@ -12,15 +12,19 @@ module ExpenseTracker
     end
 
     let(:ledger) { instance_double('ExpenseTracker::Ledger') }
-    let(:expense) { {'some' => 'data'} }
-    before do
-      allow(ledger).to receive(:record)
-        .with(expense)
-        .and_return(RecordResult.new(true, 417, nil))
-    end
+
 
     describe "POST /expenses" do
       context "when the expense is successfully recorded" do
+
+        let(:expense) { {'some' => 'data'} }
+
+        before do
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(true, 417, nil))
+        end
+
         it 'returns the expense id' do
 
         post '/expenses', JSON.generate(expense)
@@ -29,16 +33,35 @@ module ExpenseTracker
         expect(parsed).to include('expense_id' => 417)
         end
         it 'responds with a 200 (OK)' do
-        
+
           post '/expenses', JSON.generate(expense)
           expect(last_response.status).to eq(200)
 
         end
       end
 
-      context "when the expense fails validation" do
-        it 'returns an error message'
-        it 'responds with 422 (Unprocessable entity)'
+      context 'when the expense fails validation' do
+        let(:expense) { {'some' => 'data'} }
+
+        before do
+          allow(ledger).to receive(:record)
+            .with(expense)
+            .and_return(RecordResult.new(false, 417, 'Expense Incomplete'))
+        end
+
+        it 'returns an error message' do
+          post '/expenses', JSON.generate(expense)
+
+          parsed = JSON.parse(last_response.body)
+          expect(parsed).to include('error' => 'Expense Incomplete')
+        end
+
+        it 'responds with 422 (Unprocessable entity)' do
+          post '/expenses', JSON.generate(expense)
+
+          expect(last_response.status).to eq(422)
+        end
+
       end
 
     end
