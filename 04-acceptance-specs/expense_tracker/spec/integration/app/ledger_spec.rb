b996b04a3744d05ec20369d94d6559380fad7fa1
Denis Sanchez Dujota <e_scriba@hotmail.com>
2017-12-20 16:32:07 -0500

    completed the expectations for the success context

--- 04-acceptance-specs/expense_tracker/spec/integration/app/ledger_spec.rb
+++ 04-acceptance-specs/expense_tracker/spec/integration/app/ledger_spec.rb
@@ -19,10 +19,17 @@ module ExpenseTracker
         it "successfully saves the expense in the DB" do
           result = ledger.record(expense)
 
+          expect(result).to be_success
+          expect(DB[:expenses].all).to match [a_hash_including(
+            id: result.expense_id,
+            payee: 'Starbucks',
+            amount: 5.75
+            date: Date.iso8601('2017-06-10')
+            )]
         end
-
       end
-    end
 
+
+    end
   end
 end
